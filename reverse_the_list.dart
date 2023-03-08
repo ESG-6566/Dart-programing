d5318f2016edac12e5600d1e38f7507cf26929f7
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2023-03-08 16:45:44 +0330

    Update reverse_the_list.dart

--- reverse_the_list.dart
+++ reverse_the_list.dart
@@ -1,14 +1,14 @@
-reverse(var lst){
-    var reversed_list = [];
-    for( var i = 3 ; i >= 0 ; i-- ) { 
-      reversed_list.add(lst[i]);
-    } 
-    return(reversed_list);
+reverse(List lst) {
+  var reversed_list = [];
+  for (var i = lst.length-1; i >= 0; i--) {
+    reversed_list.add(lst[i]);
+  }
+  return (reversed_list);
 }
 
-void main(){
-	var myList = [24, 56, 84, 92];
-	print(reverse(myList));
+void main() {
+  var myList = [24, 56, 84, 92];
+  print(reverse(myList));
 }
 
 
