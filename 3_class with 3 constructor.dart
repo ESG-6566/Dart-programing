174e4870c0c05be9f84846c3ba13635b9964a90e
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2023-05-06 14:57:07 +0430

    ex3

---
+++ 3_class with 3 constructor.dart
@@ -0,0 +1,21 @@
+//making a class with 3 constructor.
+
+class vehicle{
+  int? wheelsNumber;
+
+  vehicle.car() {
+    this.wheelsNumber = 4;
+  }
+
+  vehicle.bice() {
+    this.wheelsNumber = 2;
+  }
+
+  vehicle.truck(int wheelsNumber) {
+    this.wheelsNumber = wheelsNumber;
+  }
+}
+
+void main() {
+  var v = new vehicle.truck(8);
+}
