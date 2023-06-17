8c8dda7c733d0e09d519fe0aa4ff4764647615cf
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2023-06-17 18:39:02 +0330

    Create map and set type.dart

---
+++ map and set type.dart
@@ -0,0 +1,28 @@
+//a class with seter and geter example
+
+class Vehicle {
+  String? model;
+  String? Manufacturer;
+  String? color;
+  double? maxSpeed;
+  double? weight;
+
+  static Set<Vehicle> allVehicles = {};
+
+  Map<String, dynamic> get AllData {
+    return {
+      "model": model,
+      "Manufacturer": Manufacturer,
+      "color": color,
+      "maxSpeed": maxSpeed,
+      "weight": weight
+    };
+  }
+}
+
+void main() {
+
+  Vehicle? car = new Vehicle();
+
+  Vehicle.allVehicles.add(car);
+}
